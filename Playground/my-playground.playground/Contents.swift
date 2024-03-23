640d87f9ebdde05cbbb1ad4c1e33adf0b55470b7
Star <i@ssstttar.com>
2024-03-23 15:20:12 +0800

    chore: variables and constants

--- Playground/my-playground.playground/Contents.swift
+++ Playground/my-playground.playground/Contents.swift
@@ -1,3 +1,44 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "rebecoco"
+name = "Keeley"
+
+let character = "Daphne"
+
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+let quote = "Then he tapped a sign saying \"Believe\" and walked away"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
