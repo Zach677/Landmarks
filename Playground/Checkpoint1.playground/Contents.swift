9c0f61dc11c93e652333be2ccaf83e8eabca0481
Star <i@ssstttar.com>
2024-03-24 18:35:12 +0800

    chore: two day

---
+++ Playground/Checkpoint1.playground/Contents.swift
@@ -0,0 +1,5 @@
+import Cocoa
+
+let Celsius = 24.0
+let Fahrenheit = Celsius * 9 / 5 + 32
+print("This is your temperature: Celsius: \(Celsius)°C and Fahrenheit: \(Fahrenheit)°F")
--- Playground/my-playground.playground/Contents.swift
+++ Playground/my-playground.playground/Contents.swift
@@ -63,3 +63,50 @@ counter /= 2
 let number = 120
 print(number.isMultiple(of: 3))
 print(120.isMultiple(of: 3))
+
+let floting = 0.1 + 0.2
+print(floting)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+var rating = 5.0
+rating *= 2
+
+let goodDogs = true
+var gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting2 = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + "gonna" + action
+print(lyric)
+
+let luguageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote2 = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name2 = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name2) and I'm \(age) years old."
+print(message)
+
+let number3 = 11
+let missionMessage = "Apollo \(number3) landed on the moon."
+
+print("5 X 5 is \(5 * 5)")
