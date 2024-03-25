16d3b4c22c1a073b5b522fe1ab41635a3b4e956d
Star <i@ssstttar.com>
2024-03-25 21:07:09 +0800

    chore: update

---
+++ Playground/arrays.playground/Contents.swift
@@ -0,0 +1,54 @@
+import Cocoa
+
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+beatles.append("Allen")
+print(beatles[5])
+
+let firstBeatle = beatles[0]
+let firstNumber = numbers[0]
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+var albums = Array<String>()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+//此法与上面一样,Array<String>()与[String]()一样
+var secondAlbums = [String]()
+secondAlbums.append("Folklore")
+secondAlbums.append("Fearless")
+secondAlbums.append("Red")
+
+var thirdAlbums = ["Folklore"]
+thirdAlbums.append("Fearless")
+thirdAlbums.append("Red")
+
+print(albums.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
