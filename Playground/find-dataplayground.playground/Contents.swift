396595f93149329ce3e4298639a0e61799d72151
Star <i@ssstttar.com>
2024-03-28 21:30:22 +0800

    chore: update

---
+++ Playground/find-dataplayground.playground/Contents.swift
@@ -0,0 +1,48 @@
+import Cocoa
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+
+// It's not certain.
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+// So This is right
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+print(employee2["status", default: "Unknown"])
+
+let hasGradeated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+print(heights.count)
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin"
+
+print(archEnemies.count)
