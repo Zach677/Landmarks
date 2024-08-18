38316ebe382030dea5385d90f95e13c73881be12
Zach <i@ssstttar.com>
2024-08-18 22:36:17 +0800

    Set up navigation between list and detail

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Star on 2024/8/18.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
