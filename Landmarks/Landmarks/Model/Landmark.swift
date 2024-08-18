197888ff6245564b0dbda1d2653df28570d72c91
Zach <i@ssstttar.com>
2024-08-18 16:33:08 +0800

    update

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,34 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Star on 2024/8/18.
+//
+
+import CoreLocation
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+    private var coordinates: Coordinates
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Star on 2024/8/18.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(fileName) in main bundle.")
+    }
+		
+		do {
+				data = try Data(contentsOf: file)
+		} catch {
+				fatalError("Couldn't load \(fileName) from main bundle:\n\(error)")
+		}
+		
+		do {
+				let decoder = JSONDecoder()
+				return try decoder.decode(T.self, from: data)
+		} catch {
+				fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
+		}
+}
