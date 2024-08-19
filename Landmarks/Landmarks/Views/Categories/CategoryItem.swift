a79667eac7a4233988cdf60f619c92dc3707bcf1
Zach <i@ssstttar.com>
2024-08-19 20:56:11 +0800

    Complete the category view

---
+++ Landmarks/Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Star on 2024/8/19.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
