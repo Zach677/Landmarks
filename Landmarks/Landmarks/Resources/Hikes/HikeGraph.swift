5b3105f4b02bff4b47f4a64d9d3128694e8303be
Zach <i@ssstttar.com>
2024-08-19 20:25:07 +0800

    Animating views and transitions

--- Landmarks/Landmarks/Resources/Hikes/HikeGraph.swift
+++ Landmarks/Landmarks/Resources/Hikes/HikeGraph.swift
@@ -7,6 +7,14 @@ The elevation, heart rate, and pace of a hike plotted on a graph.
 
 import SwiftUI
 
+extension Animation {
+    static func ripple(index: Int) -> Animation {
+        Animation.spring(dampingFraction: 0.5)
+            .speed(2)
+            .delay(0.03 * Double(index))
+    }
+}
+
 struct HikeGraph: View {
     var hike: Hike
     var path: KeyPath<Hike.Observation, Range<Double>>
@@ -28,11 +36,13 @@ struct HikeGraph: View {
         let data = hike.observations
         let overallRange = rangeOfRanges(data.lazy.map { $0[keyPath: path] })
         let maxMagnitude = data.map { magnitude(of: $0[keyPath: path]) }.max()!
-        let heightRatio = 1 - CGFloat(maxMagnitude / magnitude(of: overallRange))
+        let heightRatio =
+            1 - CGFloat(maxMagnitude / magnitude(of: overallRange))
 
         return GeometryReader { proxy in
             HStack(alignment: .bottom, spacing: proxy.size.width / 120) {
-                ForEach(Array(data.enumerated()), id: \.offset) { index, observation in
+                ForEach(Array(data.enumerated()), id: \.offset) {
+                    index, observation in
                     GraphCapsule(
                         index: index,
                         color: color,
@@ -40,6 +50,7 @@ struct HikeGraph: View {
                         range: observation[keyPath: path],
                         overallRange: overallRange
                     )
+                    .animation(.ripple(index: index))
                 }
                 .offset(x: 0, y: proxy.size.height * heightRatio)
             }
@@ -48,7 +59,7 @@ struct HikeGraph: View {
 }
 
 func rangeOfRanges<C: Collection>(_ ranges: C) -> Range<Double>
-    where C.Element == Range<Double> {
+where C.Element == Range<Double> {
     guard !ranges.isEmpty else { return 0..<0 }
     let low = ranges.lazy.map { $0.lowerBound }.min()!
     let high = ranges.lazy.map { $0.upperBound }.max()!
