818dd1899736095b09f58d20b97afbed650ea370
Zach <i@ssstttar.com>
2024-08-19 20:37:38 +0800

    Create a category list

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -9,10 +9,13 @@ import Foundation
 
 @Observable
 class ModelData {
-		var landmarks: [Landmark] = load("landmarkData.json")
-		var hikes: [Hike] = load("hikeData.json")
-}
+    var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 
+    var categories: [String: [Landmark]] {
+        Dictionary(grouping: landmarks, by: { $0.category.rawValue })
+    }
+}
 
 func load<T: Decodable>(_ fileName: String) -> T {
     let data: Data
@@ -21,17 +24,17 @@ func load<T: Decodable>(_ fileName: String) -> T {
     else {
         fatalError("Couldn't find \(fileName) in main bundle.")
     }
-		
-		do {
-				data = try Data(contentsOf: file)
-		} catch {
-				fatalError("Couldn't load \(fileName) from main bundle:\n\(error)")
-		}
-		
-		do {
-				let decoder = JSONDecoder()
-				return try decoder.decode(T.self, from: data)
-		} catch {
-				fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
-		}
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(fileName) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
+    }
 }
